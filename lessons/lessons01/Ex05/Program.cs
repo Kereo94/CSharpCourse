d1941b5ed825ef7bb8a6c49863219709b8448498
Kereo <kereon94@gmail.com>
2022-12-01 12:32:12 +0300

    Добаления 5 примера, как приветствовать определенного юзера отличным от других

---
+++ lessons/lessons01/Ex05/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("УРААА, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
