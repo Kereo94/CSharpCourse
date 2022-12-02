a3308cfbf1c4811ba83645969ce40733e665f281
Kereo <kereon94@gmail.com>
2022-12-02 11:23:04 +0300

    разбор 4 примера во второй лекции

---
+++ lessons/lesson02/Ex04/Program.cs
@@ -0,0 +1,45 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+   int count = collection.Lenght;
+   int index = 0;
+   int position = -1;
+   while(index < count)
+   {
+    if(collection[index] == find)
+    {
+        position = index;
+        break;
+    }
+    index++;
+   }
+   return position;
+}
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
