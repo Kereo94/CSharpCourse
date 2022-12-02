eee072a2c6ff99f0e120c8e94a9e4322ef632d8a
Kereo <kereon94@gmail.com>
2022-12-02 08:08:06 +0300

    создание первого примера второй лекции

---
+++ lessons/lessons02/Ex01/Program.cs
@@ -0,0 +1,18 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+int a1 = 15;
+int b1 = 21;
+int c1 = 39;
+int a2 = 12;
+int b2 = 12;
+int c2 = 33;
+int a3 = 13;
+int b3 = 23;
+int c3 = 33;
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+Console.WriteLine(max);
