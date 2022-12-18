e4c1a3057d8e4707a2b0a140780955a2201bba32
Kereo <kereon94@gmail.com>
2022-12-18 08:23:52 +0300

    Решение 4-х задач первого семинара

--- seminars/seminar01/Ex01/Program.cs
+++ seminars/seminar01/Ex01/Program.cs
@@ -1,8 +1,19 @@
-﻿int a = 2, b = 3, c = 7;
+﻿/* Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
 int max = a;
 
 if (a > max) max = a;
 if (b > max) max = b;
 if (c > max) max = c;
 
-Console.WriteLine(max);
+Console.WriteLine($"Максимальное число {max}");
---
+++ seminars/seminar01/Ex04/Program.cs
@@ -0,0 +1,26 @@
+﻿
+/*Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if(numberA == numberB)
+{
+    Console.Write($"{numberA} и {numberB} равны");
+}
+else if(numberA > numberB)
+{
+    Console.WriteLine($"Максимальное число {numberA}");
+    Console.Write($"Минимальное число {numberB}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число {numberB}");
+    Console.Write($"Минимальное число {numberA}");
+}
