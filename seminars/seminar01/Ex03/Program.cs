010065e760e79f643ed51fd7d0233690d4a9a51e
Kereo <kereon94@gmail.com>
2022-12-18 08:35:00 +0300

    Корректировка 3 примера и удаление дубликата

--- seminars/seminar01/Ex03/Program.cs
+++ seminars/seminar01/Ex03/Program.cs
@@ -3,11 +3,14 @@
 8 -> 2, 4, 6, 8
 */
 
-
-Console.Write("Введите целое число: ");
-int n = Convert.ToInt32(Console.ReadLine ());
+Console.Write("Введите целое число от 2: ");
+int number = Convert.ToInt32(Console.ReadLine ());
 int a = 1;
 int index = 1;           
+if (number < 2)
+	{
+		Console.WriteLine("Введено некорректное число. Введите корректиное значиение");
+	}
 while (true)
 {
 	if (a % 2 == 0)
@@ -15,7 +18,7 @@ while (true)
 		Console.WriteLine(a);
 		index = index + 2;
 	}
-	if (index >= n)
+	if (index >= number)
 	{
 		break;
 	}
