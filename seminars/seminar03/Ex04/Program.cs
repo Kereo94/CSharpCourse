28d56599745d23ecab57638c64583e4b1667903a
Kereo <kereon94@gmail.com>
2022-12-25 07:19:02 +0300

    Решение задач к третьему семинару

---
+++ seminars/seminar03/Ex04/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+double PointDistance(double positionfirstx, double positionfirsty, double positionfirstz, double positionsecondx, double positionsecondy, double positionsecondz)
+{
+   double result = Math.Sqrt
+(
+    (positionfirstx - positionsecondx) * (positionfirstx - positionsecondx)
+    + (positionfirsty - positionsecondy) * (positionfirsty - positionsecondy)
+    + (positionfirstz - positionsecondz) * (positionfirstz - positionsecondz)
+);
+    return result;
+}
+Console.Write("Введите координату первой точки x: ");
+double positionfirstx = double.Parse(Console.ReadLine());
+Console.Write("Введите координату первой точки y: ");
+double positionfirsty = double.Parse(Console.ReadLine());
+Console.Write("Введите координату первой точки z: ");
+double positionfirstz = double.Parse(Console.ReadLine());
+Console.Write("Введите координату второй точки x: ");
+double positionsecondx = double.Parse(Console.ReadLine());
+Console.Write("Введите координату второй точки y: ");
+double positionsecondy = double.Parse(Console.ReadLine());
+Console.Write("Введите координату второй точки z: ");
+double positionsecondz = double.Parse(Console.ReadLine());
+Console.Write($"Расстояние между двумя точками составляет: {PointDistance(positionfirstx, positionfirsty, positionfirstz, positionsecondx, positionsecondy, positionsecondz):f}");
---
+++ seminars/seminar03/Ex06/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int index  = 0;
+int result = 0;
+
+while(index < number)
+{
+    index++;
+    result = index * index * index;
+    Console.Write($"{result} ");
+}
