6d96271797489ee34880d69e97d153c0e410c3bc
Kereo <kereon94@gmail.com>
2023-01-16 14:14:54 +0300

    решение первой задачи 4 семинара

---
+++ seminars/seminar04/Ex01/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+int index = a;
+
+for (int i = 1; i < b; i++)
+{
+    index = index * a;
+}
+
+Console.Write($"При возведении числа {a} в степень {b} получается: {index}");
