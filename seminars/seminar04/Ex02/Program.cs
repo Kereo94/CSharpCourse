9a496ce6ac3e31e55f0e5aae0afbec9448649bde
Kereo <kereon94@gmail.com>
2023-01-16 16:23:51 +0300

    Решены 3 задачи 4 семинара

---
+++ seminars/seminar04/Ex02/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int sum = 0;
+
+while(number > 0)
+{
+    int num = number % 10;
+    number = number / 10;
+    sum = sum + num;
+}
+Console.Write("Сумма цифр числа равна: " + sum);
---
+++ seminars/seminar04/Ex03/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+int [] numbers = new int[8];
+Console.Write("[");
+
+for (int i = 0; i < numbers.Length; i++)
+ {
+    numbers [i] = new Random().Next(0, 11);
+    Console.Write(" " + Method (i) + " ");
+ }
+Console.Write("]");
+
+int Method (int a)
+{
+    return numbers[a];
+}
